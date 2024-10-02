f53c90bf47a9d770723f52f76560ecf5df85803d
Sushyam Nagallapati <sushyamraju@gmail.com>
2024-10-02 19:36:04 +0530

    Band Name Generator

---
+++ BandNameGenerator.py
@@ -0,0 +1,7 @@
+print("WELCOME TO THE BAND GENERATOR!!")
+
+city_name = input("What is your city name: \n")
+
+pet_name = input("What is your pet name: \n")
+
+print(f"Your BAND NAME is {city_name} {pet_name}!")
